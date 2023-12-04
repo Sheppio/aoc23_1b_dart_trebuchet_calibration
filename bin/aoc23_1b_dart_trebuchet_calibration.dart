89a8db41213541dda37b4f1d607132cca4255578
Mark Sheppard <mark_sheppard_1977@hotmail.com>
2023-12-04 15:28:44 +0000

    first commit

---
+++ bin/aoc23_1b_dart_trebuchet_calibration.dart
@@ -0,0 +1,5 @@
+import 'package:aoc23_1b_dart_trebuchet_calibration/aoc23_1b_dart_trebuchet_calibration.dart' as aoc23_1b_dart_trebuchet_calibration;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aoc23_1b_dart_trebuchet_calibration.calculate()}!');
+}
---
+++ test/aoc23_1b_dart_trebuchet_calibration_test.dart
@@ -0,0 +1,8 @@
+import 'package:aoc23_1b_dart_trebuchet_calibration/aoc23_1b_dart_trebuchet_calibration.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
