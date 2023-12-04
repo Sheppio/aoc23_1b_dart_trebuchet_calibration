93cfe2fde0d8b88cdc6b5d62b4ce883b2ca0dcd8
Mark Sheppard <mark_sheppard_1977@hotmail.com>
2023-12-04 16:40:15 +0000

    working

--- test/aoc23_1b_dart_trebuchet_calibration_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:aoc23_1b_dart_trebuchet_calibration/aoc23_1b_dart_trebuchet_calibration.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
